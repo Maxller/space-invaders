bed4f21c00039c1ff8acbadff3ff8b14df8f3fea
Massimiliano Garcia Vazquez <89055172+Maxller@users.noreply.github.com>
2024-04-17 00:23:01 +0000

    Version 3

--- makefile
+++ makefile
@@ -1,5 +1,12 @@
 bin/test : src/tui_test.cpp
 	g++ src/tui_test.cpp -obin/test -std=c++2a  -lftxui-component -lftxui-screen -lftxui-dom
 
-ejecutar : bin/test
+runTest : bin/test
 	./bin/test
+
+
+bin/space : src/main.cpp
+	g++ src/main.cpp -obin/space -std=c++2a  -lftxui-component -lftxui-screen -lftxui-dom
+
+run : bin/space
+	./bin/space
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <string>
+#include <thread>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
+using namespace std;
+using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Space Invader" << std::endl;
+    
+    Element liezo = hbox({
+        spinner(5,3) | bold
+    });
+
+    auto screen = Screen::Create(
+        Dimension::Full(),
+        Dimension::Fit(liezo)
+    );
+    Render(screen, liezo);
+    screen.Print();
+    screen.ResetPosition();
+ 
+    this_thread::sleep_for(0.1s);
+    
     return 0;
 }
